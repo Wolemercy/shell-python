72d7054abb24b5bc7221694e5d537a41ad815832
wolemercy <wolemercy@gmail.com>
2026-06-03 21:56:34 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,11 @@ def main():
 
         if command == 'exit':
             break
+
+        if len(command) >= 4 and command[:4] == 'echo':
+            print(command[4:].lstrip())
+            continue
+
         print(f"{command}: command not found")
 
 
