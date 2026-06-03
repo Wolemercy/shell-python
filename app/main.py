630be9788360a34683c92076212c4b08bc031102
wolemercy <wolemercy@gmail.com>
2026-06-03 21:58:02 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,14 +7,13 @@ def main():
 
         command = input()
 
-        if command == 'exit':
+        if command == "exit":
             break
-
-        if len(command) >= 4 and command[:4] == 'echo':
-            print(command[4:].lstrip())
+        elif command.startswith("echo "):
+            print(command[5:])
             continue
-
-        print(f"{command}: command not found")
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
